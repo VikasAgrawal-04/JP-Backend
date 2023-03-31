8b4eb9a1662bf9f24df46d51f7e883dbac35da20
Vikas <vikash.agarawal2012@gmail.com>
2023-03-31 15:35:50 +0530

    db coneection successful

--- config/database.js
+++ config/database.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
 
-
-
-const connectDatabase=()=>{
-mongoose.connect(process.env.DB_URI,{useNewUrlParser:true,useUnifiedTopology:true}).
-then((data)=>{console.log(`MongoDb Connected Ka Host ${data.connection.host}`);
-})
-//catch kar denge toh unhandeled nahi hoga na 
-//no need of this because we handeled the unhandeled promise rejection
-// .catch((err)=>{
-//     console.log(err)
-// })
-}
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((data) => {
+      console.log(`MongoDb Connected Ka Host ${data.connection.host}`);
+    })
+    .catch((err) => console.log(`No Connection ${err}`));
+};
 module.exports = connectDatabase;
